23b37d0f5948e01bc9c89bf669e3b1362096ee4a
Marek <me@mark97.net>
2017-10-31 14:36:27 +0100

    Add visible icon for member

--- lib/rails_admin_clone.rb
+++ lib/rails_admin_clone.rb
@@ -11,9 +11,13 @@ module RailsAdmin
     module Actions
       class Clone < Base
         RailsAdmin::Config::Actions.register(self)
-        
-        register_instance_option :object_level do
+
+        register_instance_option :member do
           true
+        end
+
+        register_instance_option :link_icon do
+          'icon-eye-open'
         end
       end
     end
