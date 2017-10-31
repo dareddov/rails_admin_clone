32003e11c4b8acaa07df2990d8da3823937ffa2b
Marek <me@mark97.net>
2017-10-31 14:32:59 +0100

    Init commit

---
+++ lib/rails_admin_clone.rb
@@ -0,0 +1,22 @@
+require "rails_admin_clone/engine"
+
+module RailsAdminClone
+  # Your code goes here...
+end
+
+require 'rails_admin/config/actions'
+
+module RailsAdmin
+  module Config
+    module Actions
+      class Clone < Base
+        RailsAdmin::Config::Actions.register(self)
+        
+        register_instance_option :object_level do
+          true
+        end
+      end
+    end
+  end
+end
+
---
+++ lib/rails_admin_clone/version.rb
@@ -0,0 +1,3 @@
+module RailsAdminClone
+  VERSION = '0.1.0'
+end
